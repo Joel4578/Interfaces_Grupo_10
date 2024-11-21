580c6628da0d47542ca85391ce044dafddf8054b
NicoGGomez <anelecarg@icloud.com>
2024-11-21 16:57:48 -0300

    para el parallax

---
+++ TP4/EntregaFinal/js/parallax.js
@@ -0,0 +1,7 @@
+document.addEventListener('scroll', function () {
+    const image = document.getElementById('personajeCinco');
+    const scrollPosition = window.scrollY;
+
+    image.style.right = '570';
+    
+});
