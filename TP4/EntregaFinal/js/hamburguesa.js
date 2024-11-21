2f072d06976f4004dd2e7ee2b7a8935cc63c699b
Matheo <matheopacenti2@gmail.com>
2024-11-21 20:33:29 -0300

    termine, la 1er section pero lo hice con js, si puedo a la noche lo hago con keyframe y animacion

--- TP4/EntregaFinal/js/hamburguesa.js
+++ TP4/EntregaFinal/js/hamburguesa.js
@@ -1,31 +1,24 @@
-// Obtener los elementos del menú
 const menuHamburguesa = document.querySelector('.menu-hambur');
 const menuDesplegable = document.getElementById('menu-desplegable');
 
-// Función para activar/desactivar la animación
 menuHamburguesa.addEventListener('click', () => {
-    // Cambiar el estado de la imagen del menú hamburguesa (transformarla en cruz)
     menuHamburguesa.classList.toggle('open');
     
-    // Mostrar/ocultar el menú desplegable
     menuDesplegable.classList.toggle('show');
 });
 
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) { // Ajusta el valor 50 según tus necesidades
+  if (window.scrollY > 50) { 
     header.classList.add('sticky');
   } else {
     header.classList.remove('sticky');
   }
 });
 
-// animacion para que el logo se vaya al header
-/// Selecciona el logo
 const logo = document.querySelector('.logo-inicio');
 
-// Valores límite para la posición del logo y escala
 const initialTop = 55;
 const finalTop = -131;
 const initialScale = 1;
@@ -34,22 +27,46 @@ const scrollStart = 0;
 const scrollEnd = 300;
 
 window.addEventListener('scroll', () => {
-  // Obtén el valor actual del scroll
   const scrollY = window.scrollY;
 
-  // Calcula el progreso del scroll entre 0 y 1
   let progress = (scrollY - scrollStart) / (scrollEnd - scrollStart);
-
-  // Restringe el progreso entre 0 y 1
   progress = Math.max(0, Math.min(1, progress));
 
-  // Calcula la nueva posición del logo
   const newTop = initialTop + (finalTop - initialTop) * progress;
 
-  // Calcula la nueva escala del logo
   const scale = initialScale + (finalScale - initialScale) * progress;
 
-  // Aplica la nueva posición y escala al logo
   logo.style.top = `${newTop}px`;
   logo.style.transform = `translateX(-50%) scale(${scale})`;
 });
+
+// Animacion de blur para img menos logo y figuras
+const maxRotation = -20; 
+const maxBlur = 6; 
+
+const elementos = document.querySelectorAll(
+  '.inicio img:not(.personaje-uno):not(.personaje-dos):not(.personaje-tres):not(.sombra-personaje-uno):not(.sombra-personaje-dos):not(.sombra-personaje-tres):not(.logo-inicio)'
+);
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+
+  let progress = (scrollY - scrollStart) / (scrollEnd - scrollStart);
+
+  progress = Math.max(0, Math.min(1, progress));
+
+  const rotation = maxRotation * progress;
+  const blur = maxBlur * progress;
+
+  elementos.forEach((elemento) => {
+    elemento.style.transform = `rotateX(${rotation}deg)`;
+    elemento.style.filter = `blur(${blur}px)`;
+  });
+});
+const arboles = document.querySelectorAll('.arbol-uno, .arbol-dos, .arbol-tres');
+const arbolDos = document.querySelector(".arbol-dos")
+
+const arbolInitialScale = 1; 
+const arbolFinalScale = 0.9; 
+const arbolScrollStart = 0; 
+const arbolScrollEnd = 300; 
