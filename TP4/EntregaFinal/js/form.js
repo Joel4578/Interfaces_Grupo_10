8e37de2816f81e951193798ce5dc9ca64f61d962
NicoGGomez <anelecarg@icloud.com>
2024-11-21 13:55:00 -0300

    loader

--- TP4/EntregaFinal/js/form.js
+++ TP4/EntregaFinal/js/form.js
@@ -4,8 +4,14 @@ const inputMail = document.getElementById('input-mail');
 
 btn.addEventListener('click', mostrarMensaje);
 
-function mostrarMensaje(){
-    if(!inputMail){
-        mensaje.style.display ="flex";
+function mostrarMensaje() {
+    if (!inputMail.value.trim()) { // Verifica si el valor está vacío o contiene solo espacios
+        mensaje.style.display = "none"; // Oculta el mensaje si el campo no está vacío
+    } else {
+        mensaje.style.display = "flex";
+        setTimeout(() => {
+            mensaje.style.display = "none"; // Oculta el mensaje después de 3 segundos
+        }, 3000);
+        inputMail.value = "";
     }
 }
