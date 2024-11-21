33eae3101ea895f5940ad307a093d0580fc1a366
NicoGGomez <anelecarg@icloud.com>
2024-11-21 13:39:53 -0300

    form

--- TP4/EntregaFinal/js/form.js
+++ TP4/EntregaFinal/js/form.js
@@ -1,1 +1,11 @@
 const btn = document.getElementById('btn-mail');
+const mensaje = document.getElementById('mensaje');
+const inputMail = document.getElementById('input-mail');
+
+btn.addEventListener('click', mostrarMensaje);
+
+function mostrarMensaje(){
+    if(!inputMail){
+        mensaje.style.display ="flex";
+    }
+}
